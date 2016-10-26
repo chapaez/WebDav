307940cc41f9cc5b77f6c1a873173770271647cc
Andre <dajiamap@yandex.ru>
2016-10-26 19:33:09 +0300

    preinst

--- src/WebDav/WebDavInstaller.php
+++ src/WebDav/WebDavInstaller.php
@@ -3,15 +3,33 @@ namespace Uni\WebDav;
 use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 class WebDavInstaller{
+    private function recurse_copy($src,$dst) {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while(false !== ( $file = readdir($dir)) ) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if ( is_dir($src . '/' . $file) ) {
+                    recurse_copy($src . '/' . $file,$dst . '/' . $file);
+                }
+                else {
+                    copy($src . '/' . $file,$dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
     public static function postPackageInstall(Event $event){
         $io = $event->getIO();
         $io->write("Updating =.= ");
         $extra = $event->getComposer()->getPackage()->getExtra();
-        $io->write("g =.= ".serialize($extra));
         var_dump($extra);
         //$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-        $dir = '/home/bitrix/ext_www/test.domashniy.ru/webdav';
-        echo "LOLOLOLO";
-        mkdir($dir, 0755, true);
+        $dir = $extra['bxuni/wdcache'];
+        self::recurse_copy(dir(__FILE__).'../script/',$dir);
+        /*if(!file_exists($dir)) {
+            mkdir($dir, 0755, true);
+
+        }*/
+
     }
 }
