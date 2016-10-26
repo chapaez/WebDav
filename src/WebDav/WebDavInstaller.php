de909e7d7abe7c7a1812c43b0673d425e2d6e74d
Andre <dajiamap@yandex.ru>
2016-10-26 18:53:32 +0300

    preinst

--- src/WebDav/WebDavInstaller.php
+++ src/WebDav/WebDavInstaller.php
@@ -5,7 +5,7 @@ use Composer\Installer\PackageEvent;
 class WebDavInstaller{
     public function postPackageInstall(Event $event){
         //$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-        $dir = '/home/bitrix/ext_www/test.domashniy.ru/webdav/';
+        $dir = '/home/bitrix/ext_www/test.domashniy.ru/webdav';
         echo "LOLOLOLO";
         mkdir($dir, 0755, true);
     }
