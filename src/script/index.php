8a369627429e6b525fe0efd6d49be1ef6d051eaa
Andre <dajiamap@yandex.ru>
2016-10-27 17:32:17 +0300

    url list

--- src/script/index.php
+++ src/script/index.php
@@ -1,5 +1,5 @@
 <?php
-include(dirname(__FILE__). '/../autoload.php');
+include(dirname(__FILE__). '/../include_vendor.php');
 use Uni\WebDav as UW;
 if($_POST['command']=='add')
     UW\WebDavFacade::addPostPage();
