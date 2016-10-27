3445bed1bbeb93aa5ac2cf0e22cf1477c01a1801
Andre <dajiamap@yandex.ru>
2016-10-27 13:07:57 +0300

    copy config on unpack

--- src/WebDav/WebDavInstaller.php
+++ src/WebDav/WebDavInstaller.php
@@ -40,16 +40,13 @@ class WebDavInstaller{
         $dir = $extra['bxuni/wdcache'];
         if(!file_exists($dir)) {
             mkdir($dir, 0755, true);
-            self::recurse_copy(dirname(__FILE__) . '/../script/', $dir.'/script');
-            self::recurse_copy(dirname(__FILE__) . '/../adm/', $dir.'/adm');
             copy(dirname(__FILE__) . '/config.json', $dir . '/config.json');
             copy(dirname(__FILE__) . '/urls.json', $dir . '/urls.json');
-            file_put_contents(dirname(__FILE__).'scriptdir.json',json_encode(["dir"=>$dir]));
         }
-        /*if(!file_exists($dir)) {
-            mkdir($dir, 0755, true);
+        self::recurse_copy(dirname(__FILE__) . '/../script/', $dir.'/script');
+        self::recurse_copy(dirname(__FILE__) . '/../adm/', $dir.'/adm');
 
-        }*/
+        file_put_contents(dirname(__FILE__).'scriptdir.json',json_encode(["dir"=>$dir]));
 
     }
 }
