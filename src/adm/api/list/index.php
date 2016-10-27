d0fd29d5bdc2b998966980112a142fd554eb698c
Andre <dajiamap@yandex.ru>
2016-10-27 17:45:16 +0300

    url list

--- src/adm/api/list/index.php
+++ src/adm/api/list/index.php
@@ -3,13 +3,5 @@ header('Content-Type: application/json');
 include(dirname(__FILE__). '/../../../include_vendor.php');
 use Uni\WebDav as UW;
 $urlList = UW\WebDavFacade::getUrlList();
-var_dump($urlList);
-echo json_encode([
-    ["command"=>"del","time"=>1476877601,"url"=>"/schedule/"],
-    ["command"=>"add","time"=>1476877123,"url"=>"/schedule/"],
-    ["command"=>"add","time"=>1476877651,"url"=>"/schedule/"],
-    ["command"=>"del","time"=>1476876661,"url"=>"/schedule/"],
-    ["command"=>"add","time"=>1476877555,"url"=>"/schedule/"],
-    ["command"=>"del","time"=>1412590851,"url"=>"/schedule/"],
-    ["command"=>"add","time"=>1478244013,"url"=>"/schedule/"],
-]);
+
+echo json_encode($urlList);
--- src/script/cronTask.php
+++ src/script/cronTask.php
@@ -1,5 +1,5 @@
 <?php
 set_time_limit(3600);
-include(dirname(__FILE__). '/../autoload.php');
+include(dirname(__FILE__). '/../include_vendor.php');
 use Uni\WebDav as UW;
 UW\WebDavFacade::doWebDav();
