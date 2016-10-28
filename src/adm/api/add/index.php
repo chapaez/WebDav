a128da431ac8d78bbef9273022e506822b3b6318
Andre <dajiamap@yandex.ru>
2016-10-28 15:10:59 +0300

    admin add command

--- src/adm/api/add/index.php
+++ src/adm/api/add/index.php
@@ -1,7 +1,3 @@
 <?
 header('Content-Type: application/json');
-include(dirname(__FILE__). '/../../../include_vendor.php');
-use Uni\WebDav as UW;
-$urlList = UW\WebDavFacade::getUrlList();
-
-echo json_encode($urlList);
+var_dump($_POST);
