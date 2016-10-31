7d9f621a7e8061d96658f28f18a7768fa593d223
Andre <dajiamap@yandex.ru>
2016-10-31 18:44:43 +0300

    clear urls command

--- src/adm/api/add/index.php
+++ src/adm/api/add/index.php
@@ -1,4 +1,4 @@
 <?
 include(dirname(__FILE__). '/../../../include_vendor.php');
 header('Content-Type: application/json');
-\Uni\WebDav\WebDavFacade::addCommand($_POST['command'],$_POST['url'],$_POST['recursive']);
+\Uni\WebDav\WebDavFacade::addCommand($_POST['command'],$_POST['url'],$_POST['section']);
